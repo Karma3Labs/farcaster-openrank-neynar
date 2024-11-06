6fa6ae379c9f8edf320f35765c1282299d6b0d38
Vijay Mariadassou <vijay@karma3labs.com>
2024-11-05 19:56:20 -0800

    fix: weights param not being parsed properly

--- serve/app/models/score_model.py
+++ serve/app/models/score_model.py
@@ -23,6 +23,16 @@ class Weights(NamedTuple):
   reply:int = 7
   like:int = 1
 
+  @staticmethod
+  def from_str(weights_str:str) -> Self:
+    wts = re.search(r'^([lL](\d{1,2}))?([cC](\d{1,2}))?([rR](\d{1,2}))?([yY](\d{1,2}))?$', weights_str)
+    if wts is None:
+      raise Exception("Invalid weights")
+    return Weights(like=0 if wts.group(2) is None else wts.group(2),
+                  cast=0 if wts.group(4) is None else wts.group(4),
+                  recast=0 if wts.group(6) is None else wts.group(6),
+                  reply=0 if wts.group(8) is None else wts.group(8))
+
 
 class EngagementType(StrEnum):
   V1 = '1.0'
@@ -33,13 +43,4 @@ engagement_ids = dict()
 engagement_ids[EngagementType.V1] = 3
 engagement_ids[EngagementType.V3] = 9
 
-@staticmethod
-def from_str(weights_str:str) -> Self:
-  wts = re.search(r'^([lL](\d{1,2}))?([cC](\d{1,2}))?([rR](\d{1,2}))?([yY](\d{1,2}))?$', weights_str)
-  if wts is None:
-    raise Exception("Invalid weights")
-  return Weights(like=0 if wts.group(2) is None else wts.group(2),
-                 cast=0 if wts.group(4) is None else wts.group(4),
-                 recast=0 if wts.group(6) is None else wts.group(6),
-                 reply=0 if wts.group(8) is None else wts.group(8))
 
